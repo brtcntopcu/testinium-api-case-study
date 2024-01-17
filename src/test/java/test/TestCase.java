289d112c34c258cca6815064e9c7b78fe2959eac
Berat Can TOPCU <50115134+brtcntopcu@users.noreply.github.com>
2024-01-17 12:57:15 +0300

    fix typo

--- src/test/java/test/TestCase.java
+++ src/test/java/test/TestCase.java
@@ -15,6 +15,7 @@ public class TestCase {
 
     @Test
     public void testCase() {
+        
         String boardID = boards.postCreateBoard().jsonPath().get("id"); //Create board and get board ID
         String listID = boards.getFirstBoardListID(boardID); //Get first list in board to access cards
 
@@ -25,7 +26,7 @@ public class TestCase {
 
         int rand_int1 = random.nextInt(lists.getCardListInList(listID).size()); //Get random number
 
-        cards.putUpdateCard(lists.getSpecificCardIdInList(listID,rand_int1)); //Update random car
+        cards.putUpdateCard(lists.getSpecificCardIdInList(listID,rand_int1)); //Update random card
 
         for (int i = 0; i < cardCountInList ; i++) { //Delete all cards in list
             cards.delDeleteCard(lists.getSpecificCardIdInList(listID, 0));
